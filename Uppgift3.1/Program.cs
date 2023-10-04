70c541621258d8edaa5071b74e8904b93838f186
arun06001 <arun06001@E-PW0234KE>
2023-10-04 12:24:09 +0200

    Add project files.

---
+++ Uppgift3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din ålder i hela år?");
+            byte ålder = byte.Parse(Console.ReadLine());
+
+            if (ålder < 16)
+            {
+                Console.WriteLine("Du får inte vara med i tävlingen eftersom du är för ung.");
+            }
+            else if (ålder > 19)
+            {
+                Console.WriteLine("Du får inte vara med i tävlingen eftersom du är för gammal.");
+            }
+            else
+            {
+                Console.WriteLine("Du får vara med i tävlingen!");
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
